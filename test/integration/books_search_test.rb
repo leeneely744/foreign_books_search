a44117608624519243f674a7c08097a8e872eb85
nlee <nlee9337@gmail.com>
2018-11-02 15:57:51 +0000

    add title search method test

--- test/integration/books_search_test.rb
+++ test/integration/books_search_test.rb
@@ -4,5 +4,7 @@ class BooksSearchTest < ActionDispatch::IntegrationTest
   test "should search books by title" do
     get search_path
     assert_select "input[id=?]", "title"
+    get search_path, params: { title: "test title" }
+    assert_template 'books/search'
   end
 end
