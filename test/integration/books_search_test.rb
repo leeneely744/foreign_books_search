1ca2fa05e50d1babdcb9df14eaaab07b3d07e94b
nlee <nlee9337@gmail.com>
2018-11-03 05:01:42 +0000

    title検索は部分一致にした

--- test/integration/books_search_test.rb
+++ test/integration/books_search_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class BooksSearchTest < ActionDispatch::IntegrationTest
   test "should search books by title" do
     get search_path
-    assert_select "input[id=?]", "q_title"
+    assert_select "input[id=?]", "q_title_cont"
     get search_path, params: { title: "test title" }
     assert_template 'books/search'
   end
