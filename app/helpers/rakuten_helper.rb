839f100ae89524822365f2a8355060476bae90fc
nlee <nlee9337@gmail.com>
2018-11-05 13:39:54 +0000

    メソッドを修正

--- app/helpers/rakuten_helper.rb
+++ app/helpers/rakuten_helper.rb
@@ -1,11 +1,11 @@
 module RakutenHelper
-  # 楽天APIから得た情報が1件ならそれをJSONで取得
+  # 楽天APIからのレスポンスに何件の書籍情報があるか
+  def countItems(json)
+    json['count']
+  end
+  
+  # 1件の書籍情報を取得
   def getItemInfo(json)
-      # レスポンスに'count'があるので、それが1なら成功
-      if json['count'] == 1
-          return json['Items'][0]["Item"]
-      else
-          return nil
-      end
+    return json['Items'][0]["Item"]
   end
 end
