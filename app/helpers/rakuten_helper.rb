00cf45aa1754177c1c614a257b979e7bd8d14194
nlee <nlee9337@gmail.com>
2018-11-05 13:50:17 +0000

    楽天ブックス総合検索APIにリクエストするメソッドを作成

--- app/helpers/rakuten_helper.rb
+++ app/helpers/rakuten_helper.rb
@@ -1,4 +1,17 @@
 module RakutenHelper
+  # 楽天ブックス総合検索APIに問い合わせする
+  # RBOSApi : Rakuten Books Overall Search Api
+  def requestToRBOSApiByIsbn(isbn)
+    url = 'https://app.rakuten.co.jp/services/api/BooksTotal/Search/20170404' +
+    '?format=json' +
+    '&isbnjan=' + isbn +
+    '&affiliateId=' + ENV["RAKUTEN_AFFILIATE_ID"] +
+    '&applicationId=' + ENV["RAKUTEN_APPLY_ID"]
+    
+    res = open(url)
+    return JSON.parse(res.read)
+  end
+  
   # 楽天APIからのレスポンスに何件の書籍情報があるか
   def countItems(json)
     json['count']
