8900e30d53b8fa64ca47a57841cd1155118cd2d3
nlee <nlee9337@gmail.com>
2018-11-11 20:56:57 +0000

    seedsの完成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,53 @@
 require "csv"
+require "open-uri"
+
+include RakutenHelper
 
 # バルクインサートはPostgres9.5+ でしか使えないので、そのうちバージョンアップする
 # 最初の一行はヘッダー行
+bookArray = []
+outputCsv = CSV.open("outputCsv.csv", 'w')
 CSV.foreach('db/csv/bookData.csv', headers: true) do |data|
-    puts "作業中: #{data["isbn"]}"
-   Book.create!(
-        :isbn => data["isbn"],
-        :vocabulary => data["vocabulary"],
-        :page => data["page"],
-        :official_url => data["official_url"]
-        )
+    sleep(2)
+    puts ($.).to_s + "番目の要素です: #{data["isbn"]}"
+    
+    res = requestToRBOSApiByIsbn(data["isbn"])
+    code, message = res.status
+    
+    if code != '200'
+        puts 'ステータス異常でseedを中断します: ' + code
+        puts message
+        break
+    end
+    
+    resJson = JSON.parse(res.read)
+    
+    bookCount = countItems(resJson)
+    if bookCount > 1
+        puts bookCount.to_s + "個の書籍が該当しました"
+        outputCsv.puts [bookCount, data["isbn"]]
+        next
+    elsif bookCount == 0
+        puts "該当する書籍がありませんでした"
+        outputCsv.puts [bookCount, data["isbn"]]
+        next
+    end
+    
+    bookInfo = getItemInfo(resJson)
+    
+    newBook = createBookEntity(bookInfo)
+    
+    if newBook.isbn.empty? || newBook.title.empty?
+        puts 'isbnかtitleが空なのでスキップします'
+        next
+    end
+    
+    bookArray << newBook
 end
+
+outputCsv.close
+
+puts 'bookArray.length = ' + bookArray.length.to_s
+Book.import bookArray
+
+puts 'finish !'
