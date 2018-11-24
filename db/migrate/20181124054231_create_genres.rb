074fe5b179f0ba58d521ab5f241f9b9909797e98
nlee <nlee9337@gmail.com>
2018-11-24 05:44:46 +0000

    genreをjenreとtypoしてたのを修正

--- db/migrate/20181124054231_create_genres.rb
+++ db/migrate/20181124054231_create_genres.rb
@@ -1,6 +1,6 @@
-class CreateJenres < ActiveRecord::Migration[5.2]
+class CreateGenres < ActiveRecord::Migration[5.2]
   def change
-    create_table :jenres do |t|
+    create_table :genres do |t|
       t.string :books_genre_id
       t.string :books_genre_name
       t.integer :genre_level
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_23_141219) do
+ActiveRecord::Schema.define(version: 2018_11_24_054231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 2018_11_23_141219) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "jenres", force: :cascade do |t|
+  create_table "genres", force: :cascade do |t|
     t.string "books_genre_id"
     t.string "books_genre_name"
     t.integer "genre_level"
