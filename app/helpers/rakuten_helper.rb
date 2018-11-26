f747b000498688dc77ba173d15bea4805b1f04e5
nlee <nlee9337@gmail.com>
2018-11-26 12:58:35 +0000

    コメントを追加

--- app/helpers/rakuten_helper.rb
+++ app/helpers/rakuten_helper.rb
@@ -25,6 +25,7 @@ module RakutenHelper
   
   # 指定されたURLから「ページ数」を取得する
   # URLは楽天APIのitem_urlにのみ対応
+  # パース速度が遅すぎてseeds.rbでは使えないが、一応残しておく。
   def getPageNumFromRakuten(url)
     doc = Nokogiri::HTML(open(url))
     doc.xpath('//li[@class="productInfo"]').each do |info|
