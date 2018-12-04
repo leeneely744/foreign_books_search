e866aa533cbb648d0a0b4fc458cd2e62efe54f8d
nlee <nlee9337@gmail.com>
2018-12-04 05:26:46 +0000

    ジャンル検索フォームを作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
     def search
         @q = Book.ransack(params[:q])
+        @genres = Genre.where(genre_level: "3")
         @books = @q.result(distinct: true)
     end
     
