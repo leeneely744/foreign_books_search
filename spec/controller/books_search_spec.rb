eac2cff9c2b9e3e258027dc85ca9a7d2664fa191
nlee <nlee9337@gmail.com>
2018-12-04 03:35:57 +0000

    red test で「ジャンル」を追加

--- spec/controller/books_search_spec.rb
+++ spec/controller/books_search_spec.rb
@@ -7,6 +7,10 @@ describe BooksController, type: :request do
     it "exists seaech page" do
       get search_path
       expect(response.status).to eq 200
+      expect(response.body).to include 'タイトル'
+      expect(response.body).to include 'ページ数'
+      expect(response.body).to include 'ジャンル'
+      expect(response.body).to include 'ランダム検索'
     end
   end
 end
