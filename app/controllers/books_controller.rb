41d4b387bb0dfedb976d0080266ca7831ecb1b81
nlee <nlee9337@gmail.com>
2018-12-07 09:14:32 +0000

    ランダム検索を作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,13 @@ class BooksController < ApplicationController
         @q = Book.ransack(params[:q])
         @genreGroups = GenreGroup.all
         @books = @q.result(distinct: true)
+        
+        # ランダム検索で使用する本のid
+        # idは0から始まるとは限らず、連続しているとも限らない
+        @randomId = Book
+        .select('id')
+        .where('id >= ?', rand(Book.first.id..Book.last.id))
+        .first
     end
     
     def show
