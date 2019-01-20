708105765701739fe169036663eea0a9862346c7
kohei <nlee9337@gmail.com>
2019-01-21 00:36:49 +0900

    「三」ボタンの機能実装
    #panel-btn-iconが.open  のときは、#book_search も.open(display: inline-grid) になる

--- app/assets/javascripts/sliderMenu.js
+++ app/assets/javascripts/sliderMenu.js
@@ -2,6 +2,7 @@ $(function() {
   $("#panel-btn").click(function() {
     $("#panel").slideToggle(200);
     $("#panel-btn-icon").toggleClass("open");
+    $("#book_search").toggleClass("open");
     return false;
   });
 });
