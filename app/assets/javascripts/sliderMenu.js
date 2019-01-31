ec2f2ad8309d1e2eaad76bc6bddec411dbdd9d01
kohei <nlee9337@gmail.com>
2019-01-31 15:04:32 +0900

    remove unuse javascript

--- app/assets/javascripts/sliderMenu.js
+++ app/assets/javascripts/sliderMenu.js
@@ -1,7 +1,5 @@
 $(function() {
   $("#panel-btn").click(function() {
-    $("#panel").slideToggle(200);
-    $("#panel-btn-icon").toggleClass("open");
     $(".search-forms-panel").toggleClass("open");
     return false;
   });
