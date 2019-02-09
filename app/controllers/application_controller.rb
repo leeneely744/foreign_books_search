86c5daa95895fba93a61fc4f99b5d9cd2311b9b0
kohei <nlee9337@gmail.com>
2019-02-09 18:57:24 +0900

    fix my_debug()

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
 
   private
     def my_debug(obj)
+      if not obj.nil?
+        logger.debug('クラス: ' + obj.class.to_s)
+      end
       logger.debug(obj)
     end
 end
