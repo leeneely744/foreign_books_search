eb13f0ab4d22d8f81888bffef545b118c70c5f37
kohei <nlee9337@gmail.com>
2019-02-11 17:14:42 +0900

    change app name and header string
    ヘッダーのホームボタンとアプリ名を変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title(page_title = '')
-    base_title = 'Ruby on Rails Tutorial Sample App'
+    base_title = '洋書おすすめ検索'
     if page_title.empty?
       base_title
     else
