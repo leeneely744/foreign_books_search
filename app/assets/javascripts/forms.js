96b43b16cb666b0660c663cc90c6df7b7b3b12c8
kohei <nlee9337@gmail.com>
2019-02-14 00:08:24 +0900

    parge 'turbolinkc:load'

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -2,14 +2,12 @@
 /* global jQuery */
 
 (function($){
-    $(document).on('turbolinks:load', function() {
-        /* 
-        to sort-links.
-        if involve 'asc' class, add ' の低い順' to text
-        if involve 'desc' class, add ' の高い順' to text
-        */
-        $(".sort_link.asc").append(" の低い順");
-        $(".sort_link.desc").append(" の高い順");
-        $(".sort_link").not('.asc, .desc').css({"color": "gray"});
-    });
+    /* 
+    to sort-links.
+    if involve 'asc' class, add ' の低い順' to text
+    if involve 'desc' class, add ' の高い順' to text
+    */
+    $(".sort_link.asc").append(" の低い順");
+    $(".sort_link.desc").append(" の高い順");
+    $(".sort_link").not('.asc, .desc').css({"color": "gray"});
 }(jQuery));
