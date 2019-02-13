17f7ba9fbdba5f4f8c93eac30bf9413b9e91c14b
kohei <nlee9337@gmail.com>
2019-02-14 00:15:39 +0900

    delete comment

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -2,11 +2,6 @@
 /* global jQuery */
 
 (function($){
-    /* 
-    to sort-links.
-    if involve 'asc' class, add ' の低い順' to text
-    if involve 'desc' class, add ' の高い順' to text
-    */
     $(".sort_link.asc").append(" の低い順");
     $(".sort_link.desc").append(" の高い順");
     $(".sort_link").not('.asc, .desc').css({"color": "gray"});
