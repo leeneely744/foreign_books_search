e77df8b580f519314f5a34956170943201907cf4
kohei <nlee9337@gmail.com>
2019-03-03 20:04:16 +0900

    create search form tests

--- spec/features/search_management_spec.rb
+++ spec/features/search_management_spec.rb
@@ -18,4 +18,23 @@ feature "Search management" do
     click_link '検索画面へ戻る'
     expect(page).to have_title '検索'
   end
+
+  feature 'Search form test' do
+    before do
+      visit search_path
+      fill_in 'タイトル', with: 'First'
+      select 'first genre', from: 'q[books_genre_id_eq]'
+      fill_in '最小ページ数', with: 0
+      fill_in '最大ページ数', with: 10
+      click_button '検索'
+    end
+
+    scenario 'searched First Title' do
+      expect(page).to have_content("First Title")
+    end
+
+    scenario 'not searched Second Title' do
+      expect(page).not_to have_content("Second Title")
+    end
+  end
 end
