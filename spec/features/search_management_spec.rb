e2b03ae81f3c282d2a2f65f7de714e18c4cd6c78
kohei <nlee9337@gmail.com>
2019-03-08 14:19:34 +0900

    divide tests to 3 tests
    a test is divided to 3 tests
    1; title form test
    2; genre form test
    3; page num form test

--- spec/features/search_management_spec.rb
+++ spec/features/search_management_spec.rb
@@ -19,21 +19,54 @@ feature "Search management" do
     expect(page).to have_title '検索'
   end
 
-  feature 'Search form test' do
+  feature 'page num form test' do
     before do
       visit search_path
-      fill_in 'タイトル', with: 'First'
-      select 'first genre', from: 'q[books_genre_id_eq]'
-      fill_in '最小ページ数', with: 0
-      fill_in '最大ページ数', with: 10
+      fill_in '最小ページ数', with: 10
+      fill_in '最大ページ数', with: 19
       click_button '検索'
     end
 
-    scenario 'searched First Title' do
+    it 'should exists books less than 10 pages' do
       expect(page).to have_content("First Title")
     end
 
-    scenario 'not searched Second Title' do
+    it 'should not exists books more than 20 pages' do
+      expect(page).not_to have_content("Second Title")
+    end
+
+    it 'should not exists books less than 9 pages' do
+      expect(page).not_to have_content("Third Title")
+    end
+  end
+
+  feature 'genre form test' do
+    before do
+      visit search_path
+      select 'first genre', from: 'q[books_genre_id_eq]'
+      click_button '検索'
+    end
+
+    it 'should exists books of first genre' do
+      expect(page).to have_content("First Title")
+    end
+
+    it 'should not exists books of other first genre' do
+      expect(page).not_to have_content("Second Title")
+    end
+  end
+
+  feature 'title form test' do
+    before do
+      visit search_path
+      fill_in 'タイトル', with: 'First'
+    end
+
+    it 'should exists First Title' do
+      expect(page).to have_content("First Title")
+    end
+
+    it 'should not exists Second Title' do
       expect(page).not_to have_content("Second Title")
     end
   end
