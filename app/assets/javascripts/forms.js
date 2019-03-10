a8547c5ad72581ebacad784e8cc1965c7f09c46b
kohei <nlee9337@gmail.com>
2019-03-10 19:52:39 +0900

    fix javascript
    not fix rspec yet

--- app/assets/javascripts/forms.js
+++ app/assets/javascripts/forms.js
@@ -1,8 +1,8 @@
 /* global $ */
 /* global jQuery */
 
-(function($){
+$(function() {
     $(".sort_link.asc").append(" の低い順");
     $(".sort_link.desc").append(" の高い順");
     $(".sort_link").not('.asc, .desc').css({"color": "gray"});
-}(jQuery));
+})
