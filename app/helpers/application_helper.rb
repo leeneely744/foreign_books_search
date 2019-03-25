7e9264b5ae66a1c369838be413df319eeb4eca13
kohei <nlee9337@gmail.com>
2019-03-25 15:18:30 +0900

    add disclaimer to show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,14 @@
 module ApplicationHelper
   def full_title(page_title = '')
-    base_title = '洋書おすすめ検索'
+    base_title = getApplicationTitle
     if page_title.empty?
       base_title
     else
       page_title + " | " + base_title
     end
+  end
+
+  def getApplicationTitle
+    '洋書おすすめ検索'
   end
 end
