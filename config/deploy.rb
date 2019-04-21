70580e45d05ec2800bb4d852d49d48bbe37f72d7
kohei <nlee9337@gmail.com>
2019-04-21 17:17:55 +0900

    several setting

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "first-app"
+set :repo_url, "git@github.com:gitboku/first-app.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/var/www/rails/first-app"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
