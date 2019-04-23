df7ae357893f4ef20e0612c5f4bb10b7fc802f5f
kohei <nlee9337@gmail.com>
2019-04-24 00:58:45 +0900

    create script which export all data

---
+++ script/export_tables.rb
@@ -0,0 +1,61 @@
+include BooksHelper
+
+def createRow(columnNameArray, record)
+  row = ''
+  columnNameArray.each do |column|
+    data = record[column]
+    if data.is_a? NilClass
+      row += '' + ','
+    elsif data.is_a? String
+      data.gsub!(/[\r\n]/,"")
+      data.gsub!(/[\r]/,"")
+      data.gsub!(/[\n]/,"")
+      row += data + ','
+    else
+      row += data.to_s + ','
+    end
+  end
+  return row.chop!
+end
+
+# Book csv
+books = Book.all
+bookColumnNameArray = Book.column_names
+File.open("tmp/all_book.csv", "w") do |f|
+  # ヘッダー行
+  f.puts(bookColumnNameArray.to_sentence(words_connector: ","))
+
+  books.each do |book|
+    row = createRow(bookColumnNameArray, book)
+    f.puts(row)
+    # break
+  end
+end
+
+# Genre csv
+genres = Genre.all
+genreColumnsNameArray = Genre.column_names
+File.open("tmp/all_genre.csv", "w") do |f|
+  # ヘッダー行
+  f.puts(genreColumnsNameArray.to_sentence(words_connector: ","))
+
+  genres.each do |genre|
+    row = createRow(genreColumnsNameArray, genre)
+    f.puts(row)
+    # break
+  end
+end
+
+# GenreGrouop csv
+genreGroups = GenreGroup.all
+genreGroupColumnsNameArray = GenreGroup.column_names
+File.open("tmp/all_genre_group.csv", "w") do |f|
+  # ヘッダー行
+  f.puts(genreGroupColumnsNameArray.to_sentence(words_connector: ","))
+
+  genreGroups.each do |genreGroup|
+    row = createRow(genreGroupColumnsNameArray, genreGroup)
+    f.puts(row)
+    # break
+  end
+end
