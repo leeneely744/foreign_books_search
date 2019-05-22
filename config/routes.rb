f6328f057f233e58edff8c204cdf6f6fa9678860
kohei <nlee9337@gmail.com>
2019-05-23 00:29:54 +0900

    remove past route

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,4 @@ Rails.application.routes.draw do
   # '/books/:id'でbooks#showにアクセスできることがわかる
   resources :books
   
-  if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
-  end
-  post "/graphql", to: "graphql#execute"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
