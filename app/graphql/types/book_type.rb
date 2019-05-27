7393d59ca14596941fe3b32c98eae1a1a978fe25
kohei <nlee9337@gmail.com>
2019-05-28 01:01:50 +0900

    add books method

--- app/graphql/types/book_type.rb
+++ app/graphql/types/book_type.rb
@@ -2,6 +2,7 @@ module Types
   class BookType < Types::BaseObject
     field :id, ID, null: false
     field :title, String, null: false
+    field :books_genre_id, String, null: false
     field :books_count, Integer, null: true
   end
 end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,10 +1,15 @@
 module Types
   class QueryType < Types::BaseObject
     
-    # not null, but may be empty
-    field :books, [Types::BookType], null: false
-    def books
-      Book.all
+    field :books, [Types::BookType], null: false do
+      # "required: false" DocsでNullableと表示されるだけで、
+      # 本当にNullableかどうかはresolveメソッドの引数による
+      argument :page_num_from, Integer, required: false
+      argument :page_num_to, Integer, required: false
+      argument :limit, Integer, required: false
+    end
+    def books(page_num_from: 0, page_num_to: 9999, limit: 10)
+      Book.all.limit(limit)
     end
 
     field :book, Types::BookType, null: false do
