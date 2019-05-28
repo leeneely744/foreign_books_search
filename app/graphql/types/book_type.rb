90ed4f960286595fb84ffc26ed29ea7898a5911a
kohei <nlee9337@gmail.com>
2019-05-28 23:55:03 +0900

    add fields to BookType

--- app/graphql/types/book_type.rb
+++ app/graphql/types/book_type.rb
@@ -3,6 +3,12 @@ module Types
     field :id, ID, null: false
     field :title, String, null: false
     field :books_genre_id, String, null: false
-    field :books_count, Integer, null: true
+    field :small_image_url, String, null: false
+    field :medium_image_url, String, null: false
+    field :large_image_url, String, null: false
+    field :review_count, Integer, null: false
+    field :review_aveerage, String, null: false
+    field :vocabulary, Integer, null: false
+    field :page, Integer, null: false
   end
 end
