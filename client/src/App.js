4185a33aaee085d3b5fc8847505c34e1984b039c
kohei <nlee9337@gmail.com>
2019-06-10 23:05:29 +0900

    Game Component create

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Game from './Game';
 import logo from './logo.svg';
 import './App.css';
 
@@ -11,7 +12,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <Game />
         </p>
       </div>
     );
