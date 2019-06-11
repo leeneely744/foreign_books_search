36e38e261ff82712196529775dacf0fa877ac3f6
kohei <nlee9337@gmail.com>
2019-06-11 22:28:33 +0900

    Calculate winner and modify handleClick()

--- client/src/Game.js
+++ client/src/Game.js
@@ -27,6 +27,9 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squaresTmp = this.state.squares.slice();
+    if (calculateWinner(squaresTmp) || squaresTmp[i]) {
+      return;
+    }
     squaresTmp[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squaresTmp,
@@ -44,7 +47,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -84,3 +93,23 @@ function Square(props) {
 }
 
 export default Game;
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
