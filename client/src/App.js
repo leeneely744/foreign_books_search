739d831b7be82b22b72c9643c2876acf27d2f1a0
kohei <nlee9337@gmail.com>
2019-06-20 00:53:43 +0900

    create the button request API

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+// https://qiita.com/api/v2/docs#%E3%82%BF%E3%82%B0
+const API_ENDPOINT = 'https://qiita.com/api/v2/tags';
 
 class App extends Component {
+  handleGetLatAndLng() {
+    axios
+      .get(API_ENDPOINT)
+      .then((results) => {
+        const datas = results.data;
+        console.log(datas);
+      },
+      )
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +27,14 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          <h1 className="app-title">QiitaタグAPIテスト</h1>
+          <input
+          type="button"
+          value="タグ一覧を作成日時の降順でコンソールに表示します"
+          onClick={() => this.handleGetLatAndLng()}
+        />
+        </div>
       </div>
     );
   }
