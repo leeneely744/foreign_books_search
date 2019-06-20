e4d935e2763f1131d3e2ab718887d731e0a3df11
kohei <nlee9337@gmail.com>
2019-06-21 00:45:52 +0900

    create search panel component

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import axios from 'axios';
-
-// https://qiita.com/api/v2/docs#%E3%82%BF%E3%82%B0
-const API_ENDPOINT = 'https://qiita.com/api/v2/tags';
+import Search from './Search';
 
 class App extends Component {
-  handleGetLatAndLng() {
-    axios
-      .get(API_ENDPOINT)
-      .then((results) => {
-        const datas = results.data;
-        console.log(datas);
-      },
-      )
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-
   render() {
     return (
       <div className="App">
@@ -27,14 +11,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <div className="App-intro">
-          <h1 className="app-title">QiitaタグAPIテスト</h1>
-          <input
-          type="button"
-          value="タグ一覧を作成日時の降順でコンソールに表示します"
-          onClick={() => this.handleGetLatAndLng()}
-        />
-        </div>
+        <Search />
       </div>
     );
   }
---
+++ client/src/Search.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+// https://qiita.com/api/v2/docs#%E3%82%BF%E3%82%B0
+const API_ENDPOINT = 'https://qiita.com/api/v2/tags';
+
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: null
+      ,pageFrom: 0
+    };
+  }
+
+  handleGetLatAndLng() {
+    axios
+      .get(API_ENDPOINT)
+      .then((results) => {
+        const datas = results.data;
+        console.log(datas);
+      },
+      )
+      .catch((error) => {
+        console.log(error);
+      });
+    }
+
+    render() {
+      return (
+        <div className='Search'>
+          <input type='text'></input>
+          <input type='button' value='検索' onClick={() => this.handleGetLatAndLng()}>
+          </input>
+        </div>
+      );
+    }
+}
+
+export default Search;
