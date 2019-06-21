f2c955846570ae02f381457ca8a5d26d3689f868
kohei <nlee9337@gmail.com>
2019-06-21 22:29:06 +0900

    create show component

---
+++ client/src/Show.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Show extends Component {
+  render() {
+    return (
+      <div>
+        this is resut.
+      </div>
+    );
+  }
+}
+
+export default Show;
