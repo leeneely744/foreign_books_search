f6e9afe12271f3921cf805e3dec16bcdf4a8625b
kohei <nlee9337@gmail.com>
2019-06-23 14:51:08 +0900

    Show got books information in the Show component
    Delete Search component.
    App component invoke API.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Search from './Search';
+import axios from 'axios';
 import Show from './Show';
 
+// https://qiita.com/api/v2/docs#%E3%82%BF%E3%82%B0
+// このAPIは開発中に使用する仮のAPI
+const API_ENDPOINT = 'https://qiita.com/api/v2/tags?sort=count';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [],
+      searchState: [],
+    };
+  }
+
+  handleGetLatAndLng() {
+    axios
+    .get(API_ENDPOINT)
+    .then((results) => {
+      const datas = results.data;
+      results = datas.map(data => data.id + "\n");
+      this.updateBooks(results);
+    },
+    )
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+
+  updateBooks(newBooks) {
+    console.log(newBooks);
+    this.setState({books: newBooks});
+  }
+  
   render() {
     return (
       <div className="App">
@@ -12,8 +43,16 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <Search />
-        <Show />
+        <div className='Search'>
+          <input
+           type='button'
+           value='検索'
+           onClick={() => this.handleGetLatAndLng()}
+          />
+        </div>
+        <Show 
+          books={this.state.books}
+        />
       </div>
     );
   }
--- client/src/Show.js
+++ client/src/Show.js
@@ -3,8 +3,9 @@ import React, { Component } from 'react';
 class Show extends Component {
   render() {
     return (
-      <div>
-        this is resut.
+      <div style={{whiteSpace: 'pre-line'}}>
+        this is tag list from Qiita sorted by item count.
+        <ul>{this.props.books}</ul>
       </div>
     );
   }
